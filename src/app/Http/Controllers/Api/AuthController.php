572cb6382781c9dd131d24d994aea0ed3668d17b
Vitaliy <maksimalik91@bk.ru>
2026-01-19 10:35:13 +0300

    debugging-1-done

--- src/app/Http/Controllers/Api/AuthController.php
+++ src/app/Http/Controllers/Api/AuthController.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
